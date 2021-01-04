657d5c8f7fbd5ea4bf4f7b619751297d14a2f5eb
subashchandarsiva <subashchandarsiva@gmail.com>
2021-01-04 09:49:53 +0000

    chnage in state to main App

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import './App.css';
 import Navbar from './components/Layouts/Navbar'
 import Users from './components/Users/Users'
 import React ,{Component}from 'react'
+import axios from 'axios'
 
 class App extends Component {
+  state = {
+    loading:false,
+    users:[]
+  }
+  async componentDidMount(){
+    this.setState({loading:true});
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({loading:false,users:res.data})
+
+  }
   render(){
     return (
       <div className="App">
         <Navbar title=" Repo Detective"/>
         <div className="container">
-        <Users/>
+        <Users loading={this.state.loading} users={this.state.users} />
         </div>
         
       </div>
